a888e35129396fa9c98bebe4a56ca220cfe84d67
Mike Pirnat <mpirnat@gmail.com>
2011-10-11 21:55:26 -0400

    tuning the blog models

--- hackday/blog/models.py
+++ hackday/blog/models.py
@@ -12,28 +12,37 @@ class Category(models.Model):
     name = models.CharField('category name', max_length=50)
     create_date = models.DateTimeField('date created', auto_now_add=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Tag(models.Model):
     slug = models.SlugField('tag slug', unique=True, db_index=True)
     name = models.CharField('tag name', max_length=50)
     create_date = models.DateTimeField('date created', auto_now_add=True)
 
+    def __unicode__(self):
+        return self.name
 
-class Post(models.Model):
-    title = models.CharField('title of post', max_length=200)
+
+class Entry(models.Model):
+    title = models.CharField('title of entry', max_length=255)
     slug = models.SlugField('slugified title', db_index=True,
             unique_for_date="pub_date")
-    content = models.TextField('post content')
+    content = models.TextField('entry content')
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
 
     create_date = models.DateTimeField('date created', auto_now_add=True)
-    pub_date = models.DateTimeField('date published')
-    edit_date = models.DateTimeField('date edited', auto_now=True)
+    mod_date = models.DateTimeField('date modified', auto_now=True)
+    pub_date = models.DateTimeField('date published', null=True)
 
     categories = models.ManyToManyField(Category)
     tags = models.ManyToManyField(Tag)
 
-    # TODO: models.ForeignKey on post author
+    def __unicode__(self):
+        return self.title
+
+    # TODO: models.ForeignKey on entry author
 
 
 
