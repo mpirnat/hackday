a2eaad32c6cab02416beb70a273791e11aef7cc4
Mike Pirnat <mpirnat@gmail.com>
2011-10-27 23:54:39 -0400

    Created ancillary user models that'll help drive registration/profile

--- hackday/users/models.py
+++ hackday/users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Tshirt(models.Model):
+
+    description = models.CharField(max_length=50, unique=True)
+
+    create_date = models.DateTimeField('date ceated', auto_now_add=True)
+    mod_date = models.DateTimeField('date modified', auto_now=True)
+
+
+class Diet(models.Model):
+
+    description = models.CharField(max_length=50, unique=True)
+
+    create_date = models.DateTimeField('date ceated', auto_now_add=True)
+    mod_date = models.DateTimeField('date modified', auto_now=True)
+
+
+class Location(models.Model):
+
+    description = models.CharField(max_length=50, unique=True)
+
+    create_date = models.DateTimeField('date ceated', auto_now_add=True)
+    mod_date = models.DateTimeField('date modified', auto_now=True)
+
