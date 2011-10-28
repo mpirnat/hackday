9d2197b265676fcfc6db04a13d44a6adac5731dc
Mike Pirnat <mpirnat@gmail.com>
2011-10-27 23:42:09 -0400

    First sketch of team model, many TODOs

--- hackday/teams/models.py
+++ hackday/teams/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class STATUS(object):
+    ACTIVE = 'A'
+    DISQUALIFIED = 'D'
+    DELETED = 'X'
+
+    CHOICES = (
+        (ACTIVE, 'Active'),
+        (DISQUALIFIED, 'Disqualified'),
+        (DELETED, 'Deleted'),
+    )
+
+
+class Team(models.Model):
+    name = models.CharField('name of team', max_length=255, db_index=True,
+            unique=True)
+    slug = models.SlugField('slugified team name', db_index=True, unique=True)
+    project = models.TextField('description of project')
+
+    status = models.CharField(max_length=1, choices=STATUS.CHOICES)
+
+    create_date = models.DateTimeField('date created', auto_now_add=True)
+    mod_date = models.DateTimeField('date modified', auto_now=True)
+
+    def __unicode__(self):
+        return self.name
+
+    # TODO: FK to users for creator
+    # TODO: FK to users for "captain"?
+    # TODO: many-many to users for team members
+    # TODO: FK to project category
+
