0e6f5ab40594359a98ea54d88cfc1e1e6a1f2b7f
Mike Pirnat <mpirnat@gmail.com>
2011-10-28 00:44:25 -0400

    Wired up team-to-user FKs and many-to-many relations

--- hackday/teams/models.py
+++ hackday/teams/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from charities.models import Charity
+from django.contrib.auth.models import User
 
 
 class STATUS(object):
@@ -21,15 +23,19 @@ class Team(models.Model):
 
     status = models.CharField(max_length=1, choices=STATUS.CHOICES)
 
+    creator = models.ForeignKey(User,
+            related_name="%(app_label)s_%(class)s_creator")
+    captain = models.ForeignKey(User,
+            related_name="%(app_label)s_%(class)s_captain")
+    members = models.ManyToManyField(User,
+            related_name="%(app_label)s_%(class)s_members")
+
+    # TODO: FK to Category
+#    category = models.ForeignKey(Category)
+    charity = models.ForeignKey(Charity)
+
     create_date = models.DateTimeField('date created', auto_now_add=True)
     mod_date = models.DateTimeField('date modified', auto_now=True)
 
     def __unicode__(self):
         return self.name
-
-    # TODO: FK to users for creator
-    # TODO: FK to users for "captain"?
-    # TODO: many-many to users for team members
-    # TODO: FK to project category
-    # TODO: FK to charity
-
