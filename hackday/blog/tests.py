ffbbaff26a77ec9dee53e7ea3927d7515d1dd7fe
Mike Griffith <bikegriffith@gmail.com>
2011-10-29 22:11:31 -0400

    simple test of homepage showing blog entries

--- hackday/blog/tests.py
+++ hackday/blog/tests.py
@@ -1,16 +1,9 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestWhenAccessingHomepage(TestCase):
+    fixtures = ['hello']
+
+    def test_displays_recent_blog_posts(self):
+        response = self.client.get('/')
+        self.assertTrue('Hello World!' in response.content)
