fc69e6767853cf4f6d7475f2c8be2282dd95593f
Mike Pirnat <mpirnat@gmail.com>
2012-01-02 23:51:33 -0500

    Rigged up basic password reset using django contrib hotness - issue #49

--- hackday/urls.py
+++ hackday/urls.py
@@ -19,6 +19,27 @@ urlpatterns = patterns('',
     (r'^charities/?', include('charities.urls')),
 
     # User stuff - sign up, sign in, sign out, user profiles
+    (r'^users/reset-password/?$', 'django.contrib.auth.views.password_reset',
+        {
+            'template_name': 'users/password_reset_form.html',
+            'email_template_name': 'users/password_reset_email.html',
+        }),
+    (r'^users/reset-password/requested/?$',
+        'django.contrib.auth.views.password_reset_done',
+        {
+            'template_name': 'users/password_reset_done.html',
+        }),
+    (r'^users/reset-password/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/?$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {
+            'template_name': 'users/password_reset_confirm.html'
+        }),
+    (r'^users/reset-password/done/?$',
+        'django.contrib.auth.views.password_reset_complete',
+        {
+            'template_name': 'users/password_reset_complete.html'
+        }),
+
     (r'^users/?', include('users.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- hackday/users/urls.py
+++ hackday/users/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('users.views',
-    (r'^/*$', 'index'),
-    (r'^sign-up/*$', 'sign_up'),
-    (r'^sign-in/*$', 'sign_in'),
-    (r'^sign-out/*$', 'sign_out'),
-    (r'^(?P<username>\w+)/*$', 'profile'),
-    (r'^(?P<username>\w+)/edit/*$', 'edit_profile'),
+    (r'^/?$', 'index'),
+    (r'^sign-up/?$', 'sign_up'),
+    (r'^sign-in/?$', 'sign_in'),
+    (r'^sign-out/?$', 'sign_out'),
+    (r'^(?P<username>\w+)/?$', 'profile'),
+    (r'^(?P<username>\w+)/edit/?$', 'edit_profile'),
 )
