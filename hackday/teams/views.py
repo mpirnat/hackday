a8ad0a586a7cf1fef6c4588c2097f96f1349e1b8
David Noyes <david.noyes@gmail.com>
2012-01-25 22:49:41 -0500

    https://github.com/mpirnat/hackday/issues/20
    All the necessary code to successfully create a team. After successful
    creation, user is taken to the team detail page for that team.
    
    ***NOTE: This will blow up if it tries to add a team slug that already exists
    in the database. That can be addressed later (if necessary)

--- hackday/teams/views.py
+++ hackday/teams/views.py
@@ -1,2 +1,22 @@
-from django.views.generic import DetailView, ListView
-from teams.models import Team
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from teams.forms import CreateTeamForm
+from teams.models import Team, STATUS
+
+
+@login_required()
+def create(request):
+
+    form = CreateTeamForm(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        team = form.save(commit=False)
+        team.status = STATUS.ACTIVE
+        team.creator = request.user
+        team.save(user=request.user)
+        form.save_m2m()
+
+        return HttpResponseRedirect('/teams/' + team.slug)
+
+    return render(request, 'teams/team_form.html', {'form': form})
