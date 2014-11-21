3d2ec8d10072ffe5be8d4a34c4452e425d532cad
Cory Sitko <csitko@ag.com>
2014-11-21 10:51:06 -0500

    added static root

--- hackday/settings.py
+++ hackday/settings.py
@@ -58,7 +58,8 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(CODE_ROOT, 'static_collection')
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
