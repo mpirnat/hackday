7aa37ddcb6c7621e5ece9264e00cac2ee5af4193
Mike Pirnat <mpirnat@gmail.com>
2015-07-17 10:35:23 -0400

    added new route to redirect to edit profile page (via csitko)

--- hackday/users/urls.py
+++ hackday/users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('hackday.users.views',
     url(r'^sign-in/?$', 'sign_in', name='users-sign-in'),
     url(r'^sign-out/?$', 'sign_out', name='users-sign-out'),
     url(r'^view-profile/?$', 'profile_redirect', name='users-view-profile'),
+    url(r'^edit-profile/?$', 'edit_profile_redirect', name='users-edit-profile-redirect'),
     url(r'^(?P<username>[\w,\.]+)/?$', 'profile', name='users-profile'),
     url(r'^(?P<username>[\w,\.]+)/edit/?$', 'edit_profile', name='users-edit-profile'),
 )
